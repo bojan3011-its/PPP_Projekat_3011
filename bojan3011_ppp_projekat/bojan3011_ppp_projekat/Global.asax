b1eecd416edd15a1061cc0e5863123ed46329860
bojan3011-its <51379554+bojan3011-its@users.noreply.github.com>
2019-06-05 11:22:45 +0200

    Initial commit

---
+++ bojan3011_ppp_projekat/bojan3011_ppp_projekat/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="bojan3011_ppp_projekat.MvcApplication" Language="C#" %>
---
+++ bojan3011_ppp_projekat/bojan3011_ppp_projekat/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(bojan3011_ppp_projekat.Startup))]
+namespace bojan3011_ppp_projekat
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
