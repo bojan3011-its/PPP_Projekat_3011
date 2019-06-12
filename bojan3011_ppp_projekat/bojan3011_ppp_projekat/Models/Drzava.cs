2075f75d7746fb8770e62d08470272447805ade8
bojan3011-its <51379554+bojan3011-its@users.noreply.github.com>
2019-06-12 21:32:04 +0200

    Create Drzava.cs

---
+++ bojan3011_ppp_projekat/bojan3011_ppp_projekat/Models/Drzava.cs
@@ -0,0 +1,32 @@
+namespace bojan3011_ppp_projekat.Models
+{
+    using System;
+    using System.Collections.Generic;
+    using System.ComponentModel.DataAnnotations;
+    using System.ComponentModel.DataAnnotations.Schema;
+    using System.Data.Entity.Spatial;
+
+    [Table("Drzava")]
+    public partial class Drzava
+    {
+        [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2214:DoNotCallOverridableMethodsInConstructors")]
+        public Drzava()
+        {
+            Klijents = new HashSet<Klijent>();
+            Proizvodjacs = new HashSet<Proizvodjac>();
+        }
+
+        [DatabaseGenerated(DatabaseGeneratedOption.None)]
+        public int DrzavaId { get; set; }
+
+        [Required]
+        [StringLength(50)]
+        public string Naziv { get; set; }
+
+        [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2227:CollectionPropertiesShouldBeReadOnly")]
+        public virtual ICollection<Klijent> Klijents { get; set; }
+
+        [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2227:CollectionPropertiesShouldBeReadOnly")]
+        public virtual ICollection<Proizvodjac> Proizvodjacs { get; set; }
+    }
+}
